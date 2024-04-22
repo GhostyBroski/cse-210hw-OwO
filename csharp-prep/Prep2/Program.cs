955c4f4cdac97054e481a3a2d2a1833979a5bb1f
GhostyBroski <kyleajones6789@gmail.com>
2024-04-22 13:13:18 -0600

    Grade things

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -37,10 +37,67 @@ class Program
             {
                 letter = "A";
             }
-            if (percent <= 93)
+            else if (percent <= 93)
             {
                 letter = "A-";
             }
+            else
+            {
+                letter = "A";
+            }
+        }
+
+        if (letter == "B")
+        {
+            if (percent >= 86)
+            {
+                letter = "B+";
+            }
+            else if (percent <= 83)
+            {
+                letter = "B-";
+            }
+            else
+            {
+                letter = "B";
+            }
+        }
+
+        if (letter == "C")
+        {
+            if (percent >= 76)
+            {
+                letter = "C+";
+            }
+            else if (percent <= 73)
+            {
+                letter = "C-";
+            }
+            else
+            {
+                letter = "C";
+            }
+        }
+
+        if (letter == "D")
+        {
+            if (percent >= 66)
+            {
+                letter = "D+";
+            }
+            else if (percent <= 63)
+            {
+                letter = "D-";
+            }
+            else
+            {
+                letter = "D";
+            }
+        }
+
+        if (letter == "F")
+        {
+            letter = "F";
         }
 
         Console.WriteLine($"Your grade is: {letter}");
